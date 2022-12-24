639f63fbfc258d594ea31c28e7a2a2059e9abf26
tetsuya_kojima <gangelion8@gmail.com>
2022-12-24 23:14:17 +0900

    chore: update test description

--- test/sort/bubble.test.ts
+++ test/sort/bubble.test.ts
@@ -1,27 +1,27 @@
 import { Bubble } from '../../src/sort/bubble'
 import { Selection } from '../../src/sort'
 
-test('[5, 4, 3, 2, 1] to be [1, 2, 3, 4, 5]', () => {
+test('[5, 4, 3, 2, 1] equals [1, 2, 3, 4, 5]', () => {
   const bubble = new Bubble([5, 4, 3, 2, 1])
   expect(bubble.exec()).toEqual([1, 2, 3, 4, 5])
 })
 
-test('[6, 2, 9, 4, 1] to be [1, 2, 4, 6, 9]', () => {
+test('[6, 2, 9, 4, 1] equals [1, 2, 4, 6, 9]', () => {
   const bubble = new Bubble([6, 2, 9, 4, 1])
   expect(bubble.exec()).toEqual([1, 2, 4, 6, 9])
 })
 
-test('[1, 2, 3, 4, 5] to be [1, 2, 3, 4, 5]', () => {
+test('[1, 2, 3, 4, 5] equals [1, 2, 3, 4, 5]', () => {
   const bubble = new Bubble([1, 2, 3, 4, 5])
   expect(bubble.exec()).toEqual([1, 2, 3, 4, 5])
 })
 
-test('[3, 3, 10, 2, 2] to be [2, 2, 3, 3, 10]', () => {
+test('[3, 3, 10, 2, 2] equals [2, 2, 3, 3, 10]', () => {
   const bubble = new Bubble([3, 3, 10, 2, 2])
   expect(bubble.exec()).toEqual([2, 2, 3, 3, 10])
 })
 
-test('[-3, -2, -5, 8, 2] to be [-5, -3, -2, 2, 8]', () => {
+test('[-3, -2, -5, 8, 2] equals [-5, -3, -2, 2, 8]', () => {
   const bubble = new Bubble([-3, -2, -5, 8, 2])
   expect(bubble.exec()).toEqual([-5, -3, -2, 2, 8])
 })
